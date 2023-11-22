466cf528c5a8d00fa040cfb6c112f1221cea0f43
begojara99 <pch990824@naver.com>
2023-11-22 11:32:31 +0900

    탭 이동 상대경로로 수정

---
+++ admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>honghee</title>
+	<link rel="stylesheet" type="text/css" href="css/common.css">
+	<link rel="stylesheet" type="text/css" href="css/admin.css">
+	<script src="js/home.js" type="text/javascript"></script>
+</head>
+<body>
+	<header>
+		<div id="logo">
+			<a href="./home.php">
+				<img src="./images/honghee.png" alt="홍희">
+			</a>
+		</div>
+		<a class="tab" href="./guide.php">매장 안내</a>
+		<a class="tab" href="./menu.php">메뉴 정보</a>
+		<a class="tab" href="./announce.php">공지사항</a>
+	</header>
+	<div class="login-container">
+        <h4>관리자 로그인</h4>
+        <div class="login-box">
+            <label for="id">ID:</label>
+            <input type="text" id="id" name="id" placeholder="Enter your ID">
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password">
+        </div>
+    </div>
+	<footer>
+		<p>© 2023 Simple Page</p>
+	</footer>
+
+</body>
+</html>
--- announce.php
+++ announce.php
@@ -4,20 +4,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>honghee</title>
-	<link rel="stylesheet" type="text/css" href="css/home.css">
+	<link rel="stylesheet" type="text/css" href="css/common.css">
 	<link rel="stylesheet" type="text/css" href="css/announce.css">
 	<script src="home.js" type="text/javascript"></script>
 </head>
 <body>
 	<header>
 		<div>
-			<a href="http://localhost/TP/home.php">
+			<a href="./home.php">
 				<img src="./images/honghee.png" alt="홍희">
 			</a>
 		</div>
-		<a class="tab"  href="http://localhost/TP/guide.php">매장 안내</a>
-		<a class="tab"  href="http://localhost/TP/menu.php">메뉴 정보</a>
-		<a class="tab"  href="http://localhost/TP/announce.php">공지사항</a>
+		<a class="tab"  href="./guide.php">매장 안내</a>
+		<a class="tab"  href="./menu.php">메뉴 정보</a>
+		<a class="tab"  href="./announce.php">공지사항</a>
 	</header>
 	<div class="announce-container">
 		<div class="announce-box">
